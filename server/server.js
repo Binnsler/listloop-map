c5199e52a9173e3df4b9ad344b14a569837e1c00
marczenn <jamie.m.binns@gmail.com>
2016-09-15 16:09:42 -0600

    heroku tweak

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ app.get('*', function(req, res){
   res.sendFile(path.resolve('../index.html'));
 });
 
-const port = process.env.port || 3030;
+const port = process.env.port || 3000;
 const server = http.createServer(app);
 server.listen(port, function(){
-  console.log('Listening on porto: ', port);
+  console.log('Listening on port: ', port);
 });
